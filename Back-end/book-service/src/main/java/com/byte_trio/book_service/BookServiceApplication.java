87862d7fe8733dc125e35a8d191c924bf9d1d559
valkyrie <rajarshibiswas5678@gmail.com>
2025-08-31 13:14:09 +0530

    Initialize

---
+++ Back-end/book-service/src/main/java/com/byte_trio/book_service/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.byte_trio.book_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
---
+++ Back-end/entity-service/src/test/java/com/byte_trio/entity_service/EntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byte_trio.entity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
