04ab539e2d44d144fd1c8fe18088ca316807d048
valkyrie <rajarshibiswas5678@gmail.com>
2025-08-31 23:16:11 +0530

    Api-Done

---
+++ Back-end/api-gateway/src/main/java/com/byte_trio/api_gateway/config/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.byte_trio.api_gateway.config;
+
+//import com.byte_trio.authentication_service.Repository.UserRepository;
+import com.byte_trio.api_gateway.feign.AuthenticationFeignController;
+import com.byte_trio.api_gateway.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+//    private UserRepository repo;
+//    @Autowired
+//    private void setRepo(UserRepository repo) {this.repo = repo;}
+
+    private AuthenticationFeignController feign;
+    @Autowired
+    private void setFeign(AuthenticationFeignController feign) {this.feign = feign;}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = feign.getUser(username).getBody();//repo.findById(username).orElse(null);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("USER-404");
+        }
+
+        return CustomUserDetails.initialize(user);
+    }
+}
